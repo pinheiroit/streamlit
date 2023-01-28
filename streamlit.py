67aee1c7061a6eb67d93e189835a2b4a6db6e780
CARLOS FELIPE PINHEIRO DA SILVA <38437440+pinheiroit@users.noreply.github.com>
2023-01-28 00:32:18 -0500

    Add files via upload

--- streamlit.py
+++ streamlit.py
@@ -1,14 +1,12 @@
-##Meu primeiro app
-
 import streamlit as st
-import pandas as pd
 
+left_column, right_column = st.columns(2)
+# You can use a column just like st.sidebar:
+left_column.button('Press me!')
 
-variavel = input('Digite o nome da variavel:')
-
-df = pd.DataFrame({
-    'primeiraColuna':[1,2,3,4],
-    'segundaColuna':[10,20,30,40]
-})
-
-df
+# Or even better, call Streamlit functions inside a "with" block:
+with right_column:
+    chosen = st.radio(
+        'Sorting hat',
+        ("Gryffindor", "Ravenclaw", "Hufflepuff", "Slytherin"))
+    st.write(f"You are in {chosen} house!")
